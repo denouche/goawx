e3c04c5f35079966652ec984422daae4c823dd58
Jeffrey Duroyon <jeffrey.duroyon@gmail.com>
2023-01-25 17:47:19 +0100

    end of interfacing

--- client/workflow_job_template_node_step.go
+++ client/workflow_job_template_node_step.go
@@ -8,13 +8,13 @@ import (
 )
 
 // WorkflowJobTemplateNodeStepService implements awx job template nodes apis.
-type WorkflowJobTemplateNodeStepService struct {
-	endpoint string
-	client   *Client
+type WorkflowJobTemplateNodeStepService interface {
+	ListWorkflowJobTemplateNodes(id int, params map[string]string) ([]*WorkflowJobTemplateNode, *ListWorkflowJobTemplateNodesResponse, error)
+	CreateWorkflowJobTemplateNodeStep(id int, data map[string]interface{}, params map[string]string) (*WorkflowJobTemplateNode, error)
 }
 
 // ListWorkflowJobTemplateNodes shows a list of job templates nodes.
-func (jt *WorkflowJobTemplateNodeStepService) ListWorkflowJobTemplateNodes(id int, params map[string]string) ([]*WorkflowJobTemplateNode, *ListWorkflowJobTemplateNodesResponse, error) {
+func (jt *awx) ListWorkflowJobTemplateNodes(id int, params map[string]string) ([]*WorkflowJobTemplateNode, *ListWorkflowJobTemplateNodesResponse, error) {
 	workflowJobTemplateNodesActionEndpoint := fmt.Sprintf(jt.endpoint, id)
 	return fetchWorkflowJobTemplateNode(jt.client, params, workflowJobTemplateNodesActionEndpoint)
 }
@@ -57,7 +57,7 @@ func createWorkflowJobTemplateNode(client *Client, data map[string]interface{}, 
 }
 
 // CreateWorkflowJobTemplateNodeStep will be create a template node for a existing node
-func (jt *WorkflowJobTemplateNodeStepService) CreateWorkflowJobTemplateNodeStep(id int, data map[string]interface{}, params map[string]string) (*WorkflowJobTemplateNode, error) {
+func (jt *awx) CreateWorkflowJobTemplateNodeStep(id int, data map[string]interface{}, params map[string]string) (*WorkflowJobTemplateNode, error) {
 	workflowJobTemplateNodesActionEndpoint := fmt.Sprintf(jt.endpoint, id)
 	return createWorkflowJobTemplateNode(jt.client, data, params, workflowJobTemplateNodesActionEndpoint)
 }
